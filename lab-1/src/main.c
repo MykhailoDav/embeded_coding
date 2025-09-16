e9c1f695d237082d4baf2af5fca8983bb3b997c8
Mykhailo Davydenko <davudenko.mukhailo@outlook.com>
2025-09-16 14:54:14 +0300

    added Blinker

--- lab-1/src/main.c
+++ lab-1/src/main.c
@@ -6,3 +6,23 @@ DDRB |= _BV(LED);
 PORTB |= 1 << LED;
 return 0;
 }
+
+
+//Blinker:
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PB5
+
+int main(void) {
+    DDRB |= _BV(LED);
+
+    while (1) {
+        PORTB |= (1 << LED);
+        _delay_ms(500);
+        PORTB &= ~(1 << LED);
+        _delay_ms(500);
+    }
+
+    return 0;
+}

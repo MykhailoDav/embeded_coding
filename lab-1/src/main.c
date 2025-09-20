0e18b13205499ab7b07d1c62d169af446e654c8c
MykhailoDav <davudenko.mukhailo@gmail.com>
2025-09-20 16:50:52 +0300

    Implement Morse code functionality with dot and dash functions

--- lab-1/src/main.c
+++ lab-1/src/main.c
@@ -1,4 +1,52 @@
 #include <avr/io.h>
+#include <util/delay.h>
+
+#define LED PB5
+
+void dot()
+{
+    PORTB |= (1 << LED);
+    _delay_ms(200);
+    PORTB &= ~(1 << LED);
+    _delay_ms(200);
+}
+
+void dash()
+{
+    PORTB |= (1 << LED);
+    _delay_ms(600);
+    PORTB &= ~(1 << LED);
+    _delay_ms(200);
+}
+
+int main(void)
+{
+    DDRB |= (1 << LED);
+
+    while (1)
+    {
+        // S
+        dot();
+        dot();
+        dot();
+        _delay_ms(600);
+
+        // O
+        dash();
+        dash();
+        dash();
+        _delay_ms(600);
+
+        // S
+        dot();
+        dot();
+        dot();
+
+        _delay_ms(1400);
+    }
+}
+
+/* #include <avr/io.h>
 #define LED PB5
 int main() {
 DDRB |= _BV(LED);
@@ -6,9 +54,9 @@ DDRB |= _BV(LED);
 PORTB |= 1 << LED;
 return 0;
 }
+ */
 
-
-//Blinker:
+/* //Blinker:
 #include <avr/io.h>
 #include <util/delay.h>
 
@@ -25,4 +73,4 @@ int main(void) {
     }
 
     return 0;
-}
+} */
